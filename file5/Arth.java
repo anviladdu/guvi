c3de0319230c285094801a0c161911a5304e8eef
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-07-31 11:58:46 +0530

    Create Arth.java

---
+++ file5/Arth.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Arth
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		    int sum=(n*(a+b))/2;
+		    System.out.println(sum);
+	}
+}
