61ffb3831ea4ccc7e5e37fc0539a9799aad601f5
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-08-05 14:11:53 +0530

    Create CountNumber.java

---
+++ CountNumber.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public*/
+
+ 
+class CountNumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int number = 0;
+ 
+        for(int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            if (ch >= '0' && ch <= '9')
+                number++;
+        }
+ 
+        System.out.println("Number : " + number);
+	}
+}
