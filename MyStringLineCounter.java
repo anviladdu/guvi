6d41b74ab7562dd98f43769db0c9465055de7972
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-08-05 13:36:15 +0530

    Create MyStringLineCounter.java

---
+++ MyStringLineCounter.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class MyStringLineCounter {
+ 
+    public static int getLineCount(String text){
+         
+        return text.split("[.]").length;
+    }
+     
+    public static void main(String a[]){
+     
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+        int count = getLineCount(str);
+        System.out.println("line count: "+count);
+	}
+}
