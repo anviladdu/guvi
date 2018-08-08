cdfff8ac3478b3dd29cb7684ae067b3dd0c4c190
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-08-08 11:56:16 +0530

    Create StringEx.java

---
+++ StringEx.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class StringEx
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int a=sc.nextInt();
+		 for(int i=1;i<=a;i++)
+		    {
+		    	System.out.println(s);
+		    }
+	}
+}
