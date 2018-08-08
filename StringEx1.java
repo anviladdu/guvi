058019686c888b9174f122589797fb698f7c9021
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-08-08 12:25:14 +0530

    Create StringEx1.java

---
+++ StringEx1.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class StringEx1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println(s1+s2);
+	}
+}
