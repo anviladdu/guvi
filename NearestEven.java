ca73e706f954593f9907426090551fb3a088edff
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-08-12 16:18:21 +0530

    Create  NearestEven.java

---
+++ NearestEven.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class NearestEven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+         a-=1;
+        while(a%2!=0)
+      {
+         a--;
+      }
+       System.out.print(a);
+	}
+}
