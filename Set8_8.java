4a682d0b8dbfff7cc2abcdec8f16a74158765ce6
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-08-29 14:33:24 +0530

    Create Set8_8.java

---
+++ Set8_8.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Set8_8
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int b;
+        Scanner s=new Scanner(System.in);
+       b=s.nextInt();
+       if(b%13==0){
+           System.out.println("Yes");
+       }
+ else{
+           System.out.println("No");
+ }
+	}
+}
