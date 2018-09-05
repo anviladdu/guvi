807f52719095ad716b27fc76bf71027d945acd33
anvitha.boyapati <41731197+anviladdu@users.noreply.github.com>
2018-09-05 14:55:43 +0530

    Create Set9_4.java

---
+++ Set9_4.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Set9_4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int asciiCode = ch;
+        // type casting char as int
+        int asciiValue = (int)ch;
+		System.out.print(asciiValue);
+	}
+}
